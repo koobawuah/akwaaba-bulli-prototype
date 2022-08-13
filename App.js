d46b33bb660e5af101e615abdae0105b572a5a1a
koobawuah <me@bawuahboakye.com>
2022-08-13 02:04:42 +0000

    feat: Adding and implementing StackNavigation with NavigationContainer and createStackNavigator()

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native';
 import { LoginScreen } from './screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 
 export default function App() {
+
+    const Stack = createStackNavigator();
+
   return (
-    <SafeAreaView>
-        <StatusBar />
-        <LoginScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+        <Stack.Navigator> 
+            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false, }}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
