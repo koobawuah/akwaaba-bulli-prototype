2bcc63ee01b683290825803e908e3d5c16afb2e3
koobawuah <me@bawuahboakye.com>
2022-08-13 13:49:36 +0000

    feat: Called new RootNavigation component to bring in RootNavigator

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
-import { LoginScreen } from './screens';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
+import RootNavigation from './navigation/RootNavigation';
+
 
 export default function App() {
 
-    const Stack = createStackNavigator();
-
   return (
     <NavigationContainer>
-        <Stack.Navigator> 
-            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false, }}/>
-        </Stack.Navigator>
+        <RootNavigation />
     </NavigationContainer>
   );
 }
